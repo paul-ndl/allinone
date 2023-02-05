cb122a4280c2e275639c334799dd1e320f76fcb3
Paul Nadal <paul.nadal@epfl.ch>
2023-02-05 23:07:04 +0100

    fix intents

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 from features.creator.functions import ServerCreator
 from features.music.functions import Music
@@ -8,29 +9,37 @@ from features.music.functions import Music
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 guild = os.getenv('DISCORD_GUILD')
-client = discord.Client(intents=discord.Intents.default())
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 
 """ ===== Add class features to main program ===== """
 
-creator = ServerCreator(client)
-music = Music(client)
+creator = ServerCreator(bot)
+music = Music(bot)
 
 
-@client.event
+@bot.event
 async def on_ready():
-    for guild in client.guilds:
+    for guild in bot.guilds:
         if guild.name == guild:
             break
 
-    await creator.create_roles(guild)
-    await creator.create_categories_and_channels(guild) 
+    # await creator.create_roles(guild)
+    # await creator.create_categories_and_channels(guild) 
 
-@client.event
+@bot.event
 async def on_message(message):
+    if message.author == bot.user:
+            return
+    username = str(message.author)
+    user_message = str(message.content)
+    channel = str(message.channel)
+    # print(username)
+    # print(user_message)
+    # print(channel)
     if message.content == 'hello':
         await message.channel.send("Hi there!")
-        
+
 
 """ ===== Add class features to main program ===== """
-client.run(token)
+bot.run(token)
